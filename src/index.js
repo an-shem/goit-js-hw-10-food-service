0ee01b3c379ee74f38d199b9f884426246d21e54
Andrey Shemet <an-shem@ukr.net>
2021-03-10 13:52:06 +0200

    import html

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './change-theme';
 import menuData from './menu.json';
 import menuTemplate from './templat.hbs';
 import './styles.css';
-
+import './index.html';
 // ----- menu -----
 const menuRef = document.querySelector('.js-menu');
 
